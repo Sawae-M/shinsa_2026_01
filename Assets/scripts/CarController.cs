ec6c8b1c0b6c2c51319bd8dc71ed946a4684461a
Sawae-M <mikann22442244@gmail.com>
2026-01-21 20:12:24 +0900

    移動方法修正

---
+++ Assets/scripts/CarController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float moveSpeed = 1500f;
+    public float turnSpeed = 100f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Vertical");   // W/S
+        float turnInput = Input.GetAxis("Horizontal"); // A/D
+
+        // ⭕ 正しい方法：車の「正面方向」に力を加える
+        // transform.forward は、車がどこを向いていても「車の鼻先」の方向を指します
+        rb.AddForce(transform.forward * moveInput * moveSpeed * Time.fixedDeltaTime);
+
+        // 回転も「車自身の軸」で回す
+        transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.fixedDeltaTime);
+    }
+}
